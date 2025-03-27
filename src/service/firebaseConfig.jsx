d389e099e577a04d44acf15807b8819872456c02
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-03-27 23:15:41 +0545

    feat: firebase setup

---
+++ src/service/firebaseConfig.jsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaoT1tmT2MbR83lfF0hJWWnk_hrIL7G2g",
+  authDomain: "smart-trip-ai-ba940.firebaseapp.com",
+  projectId: "smart-trip-ai-ba940",
+  storageBucket: "smart-trip-ai-ba940.firebasestorage.app",
+  messagingSenderId: "805798186447",
+  appId: "1:805798186447:web:b983cc689d9e922e0856ba",
+  measurementId: "G-9YEVRX02DF"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export constdb=getFirestore();
+// const analytics = getAnalytics(app);
